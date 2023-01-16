3bc891d76e3412c2bb54940116a8f9ed0253f3fe
Junaid Ahmad <112062309+qjunaid@users.noreply.github.com>
2023-01-16 13:08:52 +0530

    Add files via upload

---
+++ resume.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<form action="resume2.php" method="get">
+Name:<input type="text" name="txt1"> <br>
+Current Profile:<input type="text" name="txt2"> <br>
+Resume Headline:<input type="text" name="txt3"> <br>
+Experience:<input type="text" name="txt4"> <br>
+E-mail: <input type="text" name="txt5"><br>
+Mob: <input type="text" name="txt6"><br>
+Address: <input type="text" name="txt7"><br>
+LinkedIn: <input type="text" name="txt8"><br>
+Github: <input type="text" name="txt9"><br>
+<input type="submit" value="OK">
+</form>
+ </body>
+</html>
